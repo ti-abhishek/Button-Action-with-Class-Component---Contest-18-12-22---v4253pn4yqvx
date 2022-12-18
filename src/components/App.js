8a4eba7e37664ba385da50db6f39ad84ec48e027
ti-abhishek <ti.abhishek15@gmail.com>
2022-12-18 19:02:02 +0530

    n

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,23 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-class App extends Component {
-    constructor(props) {
-		super(props);
-	 this.state = {
-		text: "Hello I've learnt to use the full-stact evaluation tool. This makes me so happy" ,
-		       click : 0
-	 }	
-	};
+function App()
+{
+	const [displayMessage, setDisplayMessage] = useState(false);
 
-    render() {
-    	return(
+    return(
     		<div id="main">
-				{ /* Do not remove this main div!! */ }
-			<button id="click" onClick={() => this.setState({count: this.state.count + 1})}>Button</button>
-		<h6 id="para">{this.state.text}</h6>
+				
+			{
+				(displayMessage === false)?(null):(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)
+
+			}
+			  
+			  <button id="click" onClick={() => setDisplayMessage(true)}>Click me</button>
     		</div>
     	);
     }
-}
+
 
 
 export default App;
